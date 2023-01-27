c296c11e795bda1b37d00a784c013224141cfdc7
PaulMora <96173496+paulmora-qb@users.noreply.github.com>
2023-01-27 09:46:10 +0100

    create more steps in the inferece part

--- src/general/pipelines/inference_pipeline.py
+++ src/general/pipelines/inference_pipeline.py
@@ -3,6 +3,8 @@
 from kedro.pipeline import Pipeline, node
 
 from general.functions.preprocessing.filtering import filter_dataframe
+from general.nodes.modeling.model_inference import model_prediction
+from general.nodes.preprocessing.transformer import fit, transform
 
 filter_dataframe_node = Pipeline(
     nodes=[
@@ -13,9 +15,38 @@ filter_dataframe_node = Pipeline(
                 "reference_team": "params:reference_team",
                 "reference_season": "params:reference_season",
             },
-            outputs="inference_master_table",
+            outputs="master_table_inference",
             name="filter_master_table_inference",
-            tags=["inference"],
+            tags=["filter", "inference"],
+        )
+    ]
+)
+
+apply_imputer = Pipeline(
+    nodes=[
+        node(
+            func=transform,
+            inputs={"data": "master_table_inference", "transformer": "fitted_imputer",},
+            outputs="imputed_dataset_inference",
+            name="imputing_dataset_inference",
+            tags=["imputation", "inference"],
+        )
+    ]
+)
+
+model_prediction = Pipeline(
+    nodes=[
+        node(
+            func=model_prediction,
+            inputs={
+                "data": "imputed_dataset_inference",
+                "trained_model": "best_fitted_model",
+                "prediction_suffix": "params:model_params.prediction_suffix",
+                "prediction_proba_suffix": "params:model_params.prediction_proba_suffix",
+            },
+            outputs="model_predictions",
+            name="make_model_predictions",
+            tags=["predictions", "inference"],
         )
     ]
 )
@@ -23,6 +54,6 @@ filter_dataframe_node = Pipeline(
 
 def create_pipeline():
 
-    nodes = [filter_dataframe_node]
+    nodes = [filter_dataframe_node, apply_imputer]
 
     return Pipeline(nodes, tags=["inference"])
