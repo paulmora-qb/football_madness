21de09f9be36d0db0df942379ab48c7bd557dfcb
PaulMora <96173496+paulmora-qb@users.noreply.github.com>
2023-02-16 21:40:50 +0100

    adding the reference league

--- src/general/functions/preprocessing/filtering.py
+++ src/general/functions/preprocessing/filtering.py
@@ -5,7 +5,10 @@ from pyspark.sql import functions as f
 
 
 def filter_dataframe(
-    data: DataFrame, reference_season: str = None, reference_team: str = None
+    data: DataFrame,
+    reference_season: str = None,
+    reference_team: str = None,
+    reference_league: str = None,
 ) -> DataFrame:
     """This function filters the master table down to what is desired to be predicted
     in the end. Herein we namely filter the two column
@@ -15,15 +18,14 @@ def filter_dataframe(
             `home_team`, `away_team` and `season` in order to filter
         reference_season (str): Season for which we would like to filter
         reference_team (str): Team for which we would like to get the predictions
+        reference_league(str): League which we would like to filter
 
     Returns:
         DataFrame: Filtered dataframe
     """
 
-    if (not reference_season) and (not reference_team):
-        raise ValueError(
-            "Either reference season or reference team has to be specified"
-        )
+    if (not reference_season) and (not reference_team) and (not reference_league):
+        raise ValueError("Either reference season, team or season has to be specified")
 
     dataframe_columns = data.columns
 
@@ -45,6 +47,12 @@ def filter_dataframe(
     if reference_season:
         filtered_dataframe = filtered_dataframe.filter(
             f.col("season") == reference_season
+        )
+
+    # Filtering the league
+    if reference_league:
+        filtered_dataframe = filter_dataframe.filter(
+            f.col("league") == reference_league
         )
 
     assert filtered_dataframe.count() > 0, f"The filtered dataframe is empty"
